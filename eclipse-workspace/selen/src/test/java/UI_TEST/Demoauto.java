fe2a1411cdc99ced4985d16d050cab2ed60dca90
Vishnu Vardhan Gandla <vvishnuvardhan55@gmail.com>
2024-02-17 14:11:25 +0530

    text()

--- eclipse-workspace/selen/src/test/java/UI_TEST/Demoauto.java
+++ eclipse-workspace/selen/src/test/java/UI_TEST/Demoauto.java
@@ -2,6 +2,7 @@ package UI_TEST;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,36 +25,53 @@ class Demoauto{
 			driver=new EdgeDriver();
 		}
 		
-		driver.get("https://gandlatodo.ccbp.tech/");
+//		driver.get("https://gandlatodo.ccbp.tech/");
+//		
+//		driver.manage().window().maximize();
+//
+//		
+//		driver.findElement(By.xpath("//input[@id=\"todoUserInput\"]")).sendKeys("work1");
+//		
+//		Thread.sleep(5000);
+//		
+//		driver.findElement(By.id("addTodoButton")).click();
+//		
+//		Thread.sleep(5000);
+//		
+//		driver.findElement(By.xpath("//input[starts-with(@id,todoUserInput)]")).sendKeys("work2");
+//		
+//		Thread.sleep(5000);
+//		
+//		driver.findElement(By.tagName("i")).click();
+//		
+//		Thread.sleep(5000);
+//
+//	    driver.findElement(By.xpath("//button[contains(@id,addtodobutton)]")).click();
+//		
+//		Thread.sleep(4000);
+//		
+//		driver.findElement(By.xpath("//i[contains(@class,far)]")).click();
+//		
+//		Thread.sleep(3000);
+//		
+//		driver.close();
 		
-		driver.manage().window().maximize();
-
-		
-		driver.findElement(By.xpath("//input[@id=\"todoUserInput\"]")).sendKeys("work1");
-		
-		Thread.sleep(5000);
-		
-		driver.findElement(By.id("addTodoButton")).click();
-		
-		Thread.sleep(5000);
-		
-		driver.findElement(By.xpath("//input[starts-with(@id,todoUserInput)]")).sendKeys("work2");
-		
-		Thread.sleep(5000);
-		
-		driver.findElement(By.tagName("i")).click();
-		
-		Thread.sleep(5000);
-
-	    driver.findElement(By.xpath("//button[contains(@id,addtodobutton)]")).click();
-		
-		Thread.sleep(4000);
-		
-		driver.findElement(By.xpath("//i[contains(@class,far)]")).click();
+		driver.get("https://vtu17512.ccbp.tech/");
 		
 		Thread.sleep(3000);
 		
-		driver.close();
+		driver.manage().window().maximize();
 		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//input[@type='search']")).sendKeys("India");
+		
+		driver.findElement(By.xpath("//input[@type='search']")).sendKeys(Keys.ENTER);
+		
+		Thread.sleep(5000);
+		
+		driver.findElement(By.xpath("//a[text()='India']")).click();
+		
+		driver.close();
 	}
 }
